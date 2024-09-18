f0873ab5986f601f71370ca37ad3614bce907bf4
kfortin <kfortin@student.42.fr>
2024-09-17 20:04:48 -0400

    debut topic

--- cmdTopic.cpp
+++ cmdTopic.cpp
@@ -8,6 +8,11 @@ void Server::handleTopic(Client& client, const std::vector<std::string>& params)
         client.reply(ERR_NOTREGISTERED);
         return;
     }
+    if (params.size() > 2) {
+        std::cerr << RED << "ERR_NEEDMOREPARAMS : TOPIC <channel> <:topic>\n" << std::endl;
+        client.reply(ERR_NEEDMOREPARAMS);
+        return;
+    }
     std::string channelName = params[0];
 
     Channel* channel = findChannel(channelName);
@@ -17,7 +22,21 @@ void Server::handleTopic(Client& client, const std::vector<std::string>& params)
     }
     if (channel->getTopicOpFlag_() == true && channel->isClientOperator(channel, &client) == true)
     {
-        
+        if (params.size() == 2)
+        {
+            std::string topic = params[2];
+            if (topic[0] != ':') {
+                client.reply("Need a : before topic\n");
+                return;
+            }
+            topic.erase(0, 1);
+            channel->setTopic(topic);
+        }
+        if (params.size() == 1)
+        {
+            client.reply("Need a : before topic\n");
+            return;
+        }
     }
     std::cout << YELLOW << "Kick Topic on" << std::endl;
 }
