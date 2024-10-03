4a021958a6b53c0c79c9567d59392b3e6f872a8b
jeankersen <jeankersen@yahoo.fr>
2024-10-03 13:52:21 -0400

    client disconnection, and format

--- server.hpp
+++ server.hpp
@@ -110,8 +110,13 @@ public:
     Channel*                        findChannel(const std::string& channelName);
     std::vector<Channel>::iterator  findChannelIt(std::string name);
     void                            deleteChannel(const std::string& name);
+<<<<<<< HEAD
     void                            messagetoChannel(Client& client, const std::vector<std::string>& params);
     void                            deleteClient(int clientFd);
+=======
+    void messagetoChannel(Client& client, const std::vector<std::string>& params);
+    void    deleteClient(int clientFd);
+>>>>>>> client disconnection, and format
 
 private:
     int                                 port_;
